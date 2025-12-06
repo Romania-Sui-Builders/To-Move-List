e2f7994d737d873d627ceeedf9d8cb3170971975
raulradulescu <raul.radulescu00@e-uvt.ro>
2025-12-06 17:05:26 +0200

    Update vite.config.ts

--- scripts/vite.config.ts
+++ scripts/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: 4173,
-    allowedHosts: ['tomovelist.iseethereaper.com'],
+    port: 3000,
+    allowedHosts: ['tomovelist.iseethereaper.com', 'localhost'],
   },
 });
